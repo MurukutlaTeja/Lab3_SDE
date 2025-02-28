f8a3b6aab144ed32d6e30835d8897db2e23038e0
MurukutlaTeja <Z2050844@students.niu.edu>
2025-02-27 22:11:25 -0600

    Update FlowerBed.java

--- GardenLayout/FlowerBed.java
+++ GardenLayout/FlowerBed.java
@@ -4,32 +4,39 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import java.util.ArrayList;
 import java.util.List;
+
 public class FlowerBed implements iComposite {
-    private Rectangle rectangle; 
+    private Rectangle rectangle;
     private List<Flower> flowers;
     public FlowerBed(Point2D position) {
         flowers = new ArrayList<>();
         rectangle = new Rectangle();
-        rectangle.setX(position.getX());  
-        rectangle.setY(position.getY()); 
-        rectangle.setWidth(150);  
-        rectangle.setHeight(200);  
-        rectangle.setFill(Color.BLACK);  
-        rectangle.setStroke(Color.BLACK);  
-        rectangle.setStrokeWidth(1);  
+        rectangle.setX(position.getX());
+        rectangle.setY(position.getY());
+        rectangle.setWidth(100);
+        rectangle.setHeight(200);
+        rectangle.setFill(Color.BLACK);
+        rectangle.setStroke(Color.GREY);
+        rectangle.setStrokeWidth(1);
     }
+
     public Rectangle getRectangle() {
         return rectangle;
     }
     public void addFlower(Flower flower) {
-        flowers.add(flower);  
+        if (!flowers.contains(flower)) {
+            flowers.add(flower);
+        }
+    }
+    public void removeFlower(Flower flower) {
+        flowers.remove(flower);
     }
     @Override
     public void move(double dx, double dy) {
-        rectangle.setX(rectangle.getX() + dx);  
+        rectangle.setX(rectangle.getX() + dx);
         rectangle.setY(rectangle.getY() + dy);
         for (Flower flower : flowers) {
-            flower.move(dx, dy); 
+            flower.move(dx, dy);
         }
     }
 }
